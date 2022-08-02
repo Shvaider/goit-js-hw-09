7578a883c07441dc85c178f52bf2b608423a9551
Shvaider <97763506+Shvaider@users.noreply.github.com>
2022-08-02 13:04:19 +0000

    Deploying to gh-pages from @ Shvaider/goit-js-hw-09@f4606c459e7500571716715cb260240bc698f9fb 🚀

---
+++ 01-color-switcher.dcfba96d.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){o=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){clearInterval(o)}));var o=null}();
+//# sourceMappingURL=01-color-switcher.dcfba96d.js.map
