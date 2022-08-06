011a576f20bccbc965931ca7486089e248100ade
Evgenii Horokhov <gorohovevgeniyvalerievich@gmail.com>
2022-08-06 11:01:46 +0300

    corrected-timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,8 +1,9 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
 
-const btnStart = document.querySelector('[data-start]');
+const btnStart = document.querySelector('button[data-start]');
 btnStart.setAttribute('disabled', 'disabled');
 
 // const deadLine = new Date(2022, 7, 31);
@@ -12,31 +13,46 @@ const hoursEl = document.querySelector('[data-hours]');
 const minutesEl = document.querySelector('[data-minutes]');
 const secondsEl = document.querySelector('[data-seconds]');
 
+let choseDate = null
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    delta = selectedDates[0] <= new Date();
-    if (delta < 0) {
-      btnStart.setAttribute('disabled', 'disabled');
-      Notify.failure('Please, choose a date in the future');
-    } else if (delta > 0) {
-      return btnStart.removeAttribute('disabled', 'disabled');
+    choseDate = selectedDates[0].getTime();
+    const deltaDate = choseDate - Date.now();
+
+    if (deltaDate <= 0) {
+      btnStart.disabled = true;
+      Notiflix.Notify.failure('Please, choose a date in the future');
+    } else {
+      return btnStart.disabled = false;
     }
   },
 };
+
 flatpickr(textInput, options);
+
 btnStart.addEventListener('click', onClick);
-let timerId = null;
+
+
+
 function onClick() {
+  let timerId = null;
   timerId = setInterval(() => {
-    const { days, hours, minutes, seconds } = convertMs(delta);
-    daysEl.textContent = days;
-    hoursEl.textContent = hours;
-    minutesEl.textContent = minutes;
-    secondsEl.textContent = seconds;
+    btnStart.disabled = false;
+    const deltaDate = choseDate - Date.now();
+    const { days, hours, minutes, seconds } = convertMs(deltaDate);
+    daysEl.textContent = days < 10 ? addLeadingZero(days) : days;
+    hoursEl.textContent = hours < 10 ? addLeadingZero(hours) : hours;
+    minutesEl.textContent = minutes < 10 ? addLeadingZero(minutes) : minutes;
+    secondsEl.textContent = seconds < 10 ? addLeadingZero(seconds) : seconds;
+    btnStart.disabled = true;
+    if (deltaDate <= 0) {
+      clearInterval(timerId)
+    }
   }, options.minuteIncrement);
 }
 // const today = new Date();
@@ -53,6 +69,10 @@ function onClick() {
 // console.log("days", days);
 
 //  setInterval(timer, 1000);
+function addLeadingZero(value) {
+  const stringValue = String(value)
+  return stringValue.padStart(2, '0')
+}
 // --
 function convertMs(ms) {
   // Number of milliseconds per unit of time
